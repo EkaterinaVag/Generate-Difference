3ea4a8bb01a17e93d3c5bf651197b0b15a30c097
Ekaterina <militereva@mail.ru>
2023-08-26 14:54:55 +0300

    Add test for YAML

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,14 +3,20 @@ import path, { dirname } from 'path';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url); // абсолютый путь к файлу
+const __dirname = dirname(__filename); // абсолютный путь к директории
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename); // путь к фикстурам
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'); // чтение фикстур 
 
-test('genDiff', () => {
+test('genDiff JSON', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
+  expect(genDiff(filepath1, filepath2)).toEqual(readFile('expected_JSON_file.json'));
+});
+
+test('genDiff YAML', () => {
+  const filepath1 = getFixturePath('file1.yml');
+  const filepath2 = getFixturePath('file2.yml');
   expect(genDiff(filepath1, filepath2)).toEqual(readFile('expected_JSON_file.json'));
 });
