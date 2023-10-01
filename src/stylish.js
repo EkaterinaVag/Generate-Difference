b514a3cc0007f5be09200460bd217a77486327dd
Ekaterina <militereva@mail.ru>
2023-10-01 12:46:00 +0300

    Add new logic to the stylish.js

--- src/stylish.js
+++ src/stylish.js
@@ -1,23 +1,32 @@
-const stylish = (arrayWhithType) => {
-     arrayWhithType.map((diff) => {
-    const { type } = diff;
-    switch (type) {
-      case 'deleted':
-        return `  - ${diff.key}: ${diff.value}`;
-      case 'unchanged':
-        return `    ${diff.key}: ${diff.value}`;
-      case 'changed':
-        return (`  - ${diff.key}: ${diff.value1} \n  + ${diff.key}: ${diff.value2}`);
-      case 'added':
-        return `  + ${diff.key}: ${diff.value}`;
-      case 'nasted':
-        return 
-      default:
-        return null;
+const stylish = (tree, replacer = ' ', spacesCount = 4) => {
+  const iter = (node, depth) => {
+    if (typeof node !== 'object' || node === null) {
+      return `${node}`;
     }
-  });
 
-  return `{\n${result.join('\n')}\n}`;
+    const currentSpace = spacesCount * depth;
+    const ident = replacer.repeat((currentSpace) - 2);
+  
+    const result = node.map((item) => {
+      const { type } = item;
+      switch (type) {
+        case 'deleted':
+          return `${ident}- ${item.key}: ${item.value}`;
+        case 'unchanged':
+          return `${ident}  ${item.key}: ${item.value}`;
+        case 'changed':
+          return (`${ident}- ${item.key}: ${item.value1} \n${ident}+ ${item.key}: ${item.value2}`);
+        case 'added':
+          return `${ident}+ ${item.key}: ${item.value}`;
+        case 'nasted':
+          return `${ident}  ${item.key}: ${iter(item.value, depth + 1)}`;
+        default:
+          return null;
+      }
+    }); 
+    return `{\n${result.join('\n')}\n}`;   
+  };
+  return iter(tree, 1);
 };
 
 export default stylish;
