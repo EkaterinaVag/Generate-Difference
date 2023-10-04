f65d17817cb4a00f398bc796b28b9042522b795a
Ekaterina <militereva@mail.ru>
2023-10-04 11:04:41 +0300

    Add the stringify function to the stylish.js

--- src/stylish.js
+++ src/stylish.js
@@ -1,3 +1,18 @@
+const stringify = (node, depth) => {
+  if (typeof node !== 'object' || node === null) {
+    return `${node}`;
+  }
+  const replacer = ' ';
+  const spacesCount = 4;
+  const currentSpace = spacesCount * depth;
+  const ident = replacer.repeat((currentSpace) - 2);
+
+  const array = Object.entries(node);
+  const result = array.map(([key, value]) => `${ident}  ${key}: ${stringify(value, depth + 1)}`);
+
+  return `{\n${result.join('\n')}\n  ${ident}}`;
+};
+
 const stylish = (tree, replacer = ' ', spacesCount = 4) => {
   const iter = (node, depth) => {
     if (typeof node !== 'object' || node === null) {
@@ -11,20 +26,20 @@ const stylish = (tree, replacer = ' ', spacesCount = 4) => {
       const { type } = item;
       switch (type) {
         case 'deleted':
-          return `${ident}- ${item.key}: ${item.value}`;
+          return `${ident}- ${item.key}: ${stringify(item.value)}`;
         case 'unchanged':
-          return `${ident}  ${item.key}: ${item.value}`;
+          return `${ident}  ${item.key}: ${stringify(item.value)}`;
         case 'changed':
-          return (`${ident}- ${item.key}: ${item.value1} \n${ident}+ ${item.key}: ${item.value2}`);
+          return (`${ident}- ${item.key}: ${stringify(item.value1)} \n${ident}+ ${item.key}: ${stringify(item.value2)}`);
         case 'added':
-          return `${ident}+ ${item.key}: ${item.value}`;
-        case 'nasted':
-          return `${ident}  ${item.key}: ${iter(item.value, depth + 1)}`;
+          return `${ident}+ ${item.key}: ${stringify(item.value)}`;
+        case 'nested':
+          return `${ident}  ${item.key}: ${iter(item.children, depth + 1)}${ident}`;
         default:
           return null;
       }
     }); 
-    return `{\n${result.join('\n')}\n}`;   
+    return `{\n${result.join('\n')}\n}`;
   };
   return iter(tree, 1);
 };
