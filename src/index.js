37f17f7d5a4bb2825e3d6a602aaf39b3857cd723
Ekaterina <militereva@mail.ru>
2023-10-05 17:08:12 +0300

    Divide into parts index.js

--- src/index.js
+++ src/index.js
@@ -1,67 +1,13 @@
 import fs from 'fs';
 import path from 'path';
-import _ from 'lodash';
 import getParsedData from './parsers.js';
-
-const compare = (data1, data2) => {
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const sortKeys = _.sortBy(_.union(keys1, keys2));
-
-  const arrayWhithType = sortKeys.map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
-    if (!Object.hasOwn(data1, key)) {
-      return {
-        key,
-        value: value2,
-        type: 'added',
-      };
-    } else if (!Object.hasOwn(data2, key)) {
-      return {
-        key,
-        value: value1,
-        type: 'deleted',
-      };
-    } else if (data1[key] !== data2[key]) {
-      return {
-        key,
-        value1,
-        value2,
-        type: 'changed',
-      };
-    } else {
-      return {
-        key,
-        value: value1,
-        type: 'unchanged',
-      };
-    }
-  });
-
-  const result = arrayWhithType.map((diff) => {
-    const { type } = diff;
-    switch (type) {
-      case 'deleted':
-        return `  - ${diff.key}: ${diff.value}`;
-      case 'unchanged':
-        return `    ${diff.key}: ${diff.value}`;
-      case 'changed':
-        return (`  - ${diff.key}: ${diff.value1} \n  + ${diff.key}: ${diff.value2}`);
-      case 'added':
-        return `  + ${diff.key}: ${diff.value}`;
-      default:
-        return null;
-    }
-  });
-
-  return `{\n${result.join('\n')}\n}`;
-};
+import compare from './compare.js';
+import stylish from './stylish.js';
 
 const readFile = (filePath) => fs.readFileSync(path.resolve(process.cwd(), filePath), 'utf8');
 const getExt = (filePath) => path.extname(filePath);
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const file1 = readFile(filepath1);
   const file2 = readFile(filepath2);
   const ext1 = getExt(filepath1);
@@ -69,7 +15,8 @@ const genDiff = (filepath1, filepath2) => {
 
   const obj1 = getParsedData(file1, ext1);
   const obj2 = getParsedData(file2, ext2);
-  return compare(obj1, obj2);
+  const tree = compare(obj1, obj2);
+  return stylish(tree);
 };
 
 export default genDiff;
