09a4cb126247aca5123797ccd18b703633636638
Ekaterina <militereva@mail.ru>
2023-10-05 17:11:02 +0300

    Fix "format" to "ext" in return of the func

--- src/parsers.js
+++ src/parsers.js
@@ -1,17 +1,16 @@
 import yaml from 'js-yaml';
 
-
 const getParsedData = (data, ext) => {
-    switch (ext) {
-        case '.json': 
-            return JSON.parse(data);
-        case '.yml':
-            return yaml.load(data);
-        case '.yaml': 
-            return yaml.load(data);
-        default:
-            throw new Error(`Format ${format} - is incorrect`);
-    }
-}
+  switch (ext) {
+    case '.json':
+      return JSON.parse(data);
+    case '.yml':
+      return yaml.load(data);
+    case '.yaml':
+      return yaml.load(data);
+    default:
+      throw new Error(`Format ${ext} - is incorrect`);
+  }
+};
 
 export default getParsedData;
