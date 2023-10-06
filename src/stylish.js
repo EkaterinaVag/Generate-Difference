8b40a10828ec1edc68dccebed4f402bee8662562
Ekaterina <militereva@mail.ru>
2023-10-06 11:53:37 +0300

    Fix idents in the stylish formatter

--- src/stylish.js
+++ src/stylish.js
@@ -5,10 +5,10 @@ const stringify = (node, depth) => {
   const replacer = ' ';
   const spacesCount = 4;
   const currentSpace = spacesCount * depth;
-  const ident = replacer.repeat((currentSpace) - 2);
+  const ident = replacer.repeat(currentSpace - 2);
 
   const array = Object.entries(node);
-  const result = array.map(([key, value]) => `${ident}  ${key}: ${stringify(value, depth + 1)}`);
+  const result = array.map(([key, value]) => `${ident}      ${key}: ${stringify(value, depth + 1)}`);
 
   return `{\n${result.join('\n')}\n  ${ident}}`;
 };
@@ -20,7 +20,8 @@ const stylish = (tree, replacer = ' ', spacesCount = 4) => {
     }
 
     const currentSpace = spacesCount * depth;
-    const ident = replacer.repeat((currentSpace) - 2);
+    const ident = replacer.repeat(currentSpace - 2);
+    const closedIdent = replacer.repeat(currentSpace - 4)
   
     const result = node.map((item) => {
       const { type } = item;
@@ -30,16 +31,16 @@ const stylish = (tree, replacer = ' ', spacesCount = 4) => {
         case 'unchanged':
           return `${ident}  ${item.key}: ${stringify(item.value, depth)}`;
         case 'changed':
-          return (`${ident}- ${item.key}: ${stringify(item.value1, depth)} \n${ident}+ ${item.key}: ${stringify(item.value2, depth)}`);
+          return (`${ident}- ${item.key}: ${stringify(item.value1, depth)}\n${ident}+ ${item.key}: ${stringify(item.value2, depth)}`);
         case 'added':
           return `${ident}+ ${item.key}: ${stringify(item.value, depth)}`;
         case 'nested':
-          return `${ident}  ${item.key}: ${iter(item.children, depth + 1)}${ident}`;
+          return `${ident}  ${item.key}: ${iter(item.children, depth + 1)}`;
         default:
           return null;
       }
     }); 
-    return `{\n${result.join('\n')}\n}`;
+    return `{\n${result.join('\n')}\n${closedIdent}}`;
   };
   return iter(tree, 1);
 };
