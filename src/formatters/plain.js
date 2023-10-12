d80757d389e0c101b44d3676825c9345293bc3f1
Ekaterina <militereva@mail.ru>
2023-10-12 12:15:13 +0300

    Add plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,27 +1,33 @@
 const valueType = (value) => {
     if (typeof value !== 'object' || value === null) {
-        return `${value}`;
+        return typeof value === 'string' ? `'${value}'` : `${value}`;
       }
       return `[complex value]`;
 };
 
 const plain = (tree) => {
-    tree.map((item) => {
-    const { type } = item;
-    switch (type) {
-      case 'deleted':
-        return `Property '${item.key}' was removed`;
-      case 'unchanged':
-        return `Property '${item.key}' was unchanged`;
-      case 'changed':
-        return `Property '${item.key}' was updated. From '${valueType(item.value1)}' to '${valueType(item.value2)}'`;
-      case 'added':
-        return `Property '${item.key}' was added with value: ${valueType(item.value2)}`;
-      case 'nested':
-        return ;
-      default:
-        return null;
-    }
-})
+  const iter = (node, keyName = '') => {
+    const result = node.filter((item) => item.type !== 'unchanged')
+    .map((item) => {
+        const { type } = item;
+        const keyPath = [...keyName, item.key].join('');
+        switch (type) {
+          case 'deleted':
+            return `Property '${keyPath}' was removed`;
+          case 'changed':
+            return `Property '${keyPath}' was updated. From ${valueType(item.value1)} to ${valueType(item.value2)}`;
+          case 'added':
+            return `Property '${keyPath}' was added with value: ${valueType(item.value)}`;
+          case 'nested':
+            return iter(item.children, `${keyPath}.`);
+          default:
+            return null;
+        }
+    });
+    return result.join('\n');
+  };
+  return iter(tree, '');
 };
+
+export default plain;
  
