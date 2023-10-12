2dabc3fe26f4af6075af25166adc44fee9127285
Ekaterina <militereva@mail.ru>
2023-10-12 12:19:24 +0300

    Add the ability to choose format

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import getParsedData from './parsers.js';
 import compare from './compare.js';
-import stylish from './stylish.js';
+import formatter from './formatters/index.js';
 
 const readFile = (filePath) => fs.readFileSync(path.resolve(process.cwd(), filePath), 'utf8');
 const getExt = (filePath) => path.extname(filePath);
@@ -16,7 +16,7 @@ const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const obj1 = getParsedData(file1, ext1);
   const obj2 = getParsedData(file2, ext2);
   const tree = compare(obj1, obj2);
-  return stylish(tree);
+  return formatter(tree, format);
 };
 
 export default genDiff;
