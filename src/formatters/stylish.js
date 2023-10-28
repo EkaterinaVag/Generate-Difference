d8b723e6dce4cd1339cdfce820ce9564bdacdfdb
Ekaterina <militereva@mail.ru>
2023-10-28 13:02:19 +0300

    Get indent in a separate function

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,46 +1,45 @@
+const indent = (depth) => {
+  const replacer = ' ';
+  const spacesCount = 4;
+  const currentSpace = spacesCount * depth;
+  return replacer.repeat(currentSpace - 2);
+};
+
 const stringify = (node, depth) => {
   if (typeof node !== 'object' || node === null) {
     return `${node}`;
   }
-  const replacer = ' ';
-  const spacesCount = 4;
-  const currentSpace = spacesCount * depth;
-  const ident = replacer.repeat(currentSpace - 2);
 
   const array = Object.entries(node);
-  const result = array.map(([key, value]) => `${ident}      ${key}: ${stringify(value, depth + 1)}`);
+  const result = array.map(([key, value]) => `${indent(depth)}  ${key}: ${stringify(value, depth + 1)}`);
 
-  return `{\n${result.join('\n')}\n  ${ident}}`;
+  return `{\n${result.join('\n')}\n  ${indent(depth)}}`;
 };
 
-const stylish = (tree, replacer = ' ', spacesCount = 4) => {
+const stylish = (tree) => {
   const iter = (node, depth) => {
     if (typeof node !== 'object' || node === null) {
       return `${node}`;
     }
 
-    const currentSpace = spacesCount * depth;
-    const ident = replacer.repeat(currentSpace - 2);
-    const closedIdent = replacer.repeat(currentSpace - 4);
-
     const result = node.map((item) => {
       const { type } = item;
       switch (type) {
         case 'deleted':
-          return `${ident}- ${item.key}: ${stringify(item.value, depth)}`;
+          return `${indent(depth)}- ${item.key}: ${stringify(item.value, depth)}`;
         case 'unchanged':
-          return `${ident}  ${item.key}: ${stringify(item.value, depth)}`;
+          return `${indent(depth)}  ${item.key}: ${stringify(item.value, depth)}`;
         case 'changed':
-          return (`${ident}- ${item.key}: ${stringify(item.value1, depth)}\n${ident}+ ${item.key}: ${stringify(item.value2, depth)}`);
+          return (`${indent(depth)}- ${item.key}: ${stringify(item.value1, depth)}\n${indent(depth)}+ ${item.key}: ${stringify(item.value2, depth)}`);
         case 'added':
-          return `${ident}+ ${item.key}: ${stringify(item.value, depth)}`;
+          return `${indent(depth)}+ ${item.key}: ${stringify(item.value, depth)}`;
         case 'nested':
-          return `${ident}  ${item.key}: ${iter(item.children, depth + 1)}`;
+          return `${indent(depth)}  ${item.key}: ${iter(item.children, depth + 1)}`;
         default:
           throw new Error(`Unknown type: '${type}'!`);
       }
     });
-    return `{\n${result.join('\n')}\n${closedIdent}}`;
+    return `{\n${result.join('\n')}\n${indent(depth - 2)}}`;
   };
   return iter(tree, 1);
 };
